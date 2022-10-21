ed97936b3927204e0899640af4d83e41c25836f7
Alfabit7 <tritg@yandex.ru>
2022-10-21 21:40:15 +0300

    change folder task 19

--- Lesson3/task19/Program.cs
+++ Lesson3/task19/Program.cs
@@ -4,36 +4,32 @@
 // 12821 -> да
 // 23432 -> да
 
-// Console.Clear();
-// Console.WriteLine("enter a five digit number: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-// while (number < 9999 || number > 99999)
-// {
-//     Console.WriteLine("You not enter five digit");
-//     Console.WriteLine("enter a five digit number: ");
-//     number = Convert.ToInt32(Console.ReadLine());
-// }
-
-// // Сравниваем первое число с последним и  второе число с предпоследним 
-
-// if (number / 10000 == number % 10 && number / 1000 / 10 % 10 == number % 10)
-// {
-
-//     Console.WriteLine("this is number a palindrom");
-// }
-
-// else
-// {
-//     Console.WriteLine("this number is not a palindrom ");
-
-
-// }
-
-// Решения для любого вводимого числа 
-
 Console.Clear();
-Console.WriteLine("enter number: ");
+Console.WriteLine("enter a five digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 9999 || number > 99999)
+{
+    Console.WriteLine("You not enter five digit");
+    Console.WriteLine("enter a five digit number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+// Сравниваем первое число с последним и  второе число с предпоследним 
+
+if (number / 10000 == number % 10 && number / 1000 / 10 % 10 == number % 10)
+{
+
+    Console.WriteLine("this is number a palindrom");
+}
+
+else
+{
+    Console.WriteLine("this number is not a palindrom ");
+
+
+}
+
+
 
 
