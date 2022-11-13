080a2d73290d9d49d2c4245bc6202cb8ae6692fa
Alfabit7 <tritg@yandex.ru>
2022-11-13 12:06:58 +0300

    change code for field for teteris

--- Additionak_tasks/test/Program.cs
+++ Additionak_tasks/test/Program.cs
@@ -1,6 +1,8 @@
-﻿// Console.Clear();
-Console.WriteLine();
+﻿Console.Clear();
+Console.WriteLine("         TETRIS GAME");
 
+// Поле для тетриса 
+/*
 string[,] playField = new string[,]
 {
     { "1", "2", "3", "4", "5", "6","7"},
@@ -15,28 +17,54 @@ string[,] playField = new string[,]
 
 void ShowArray(string[,] arr)
 {
-
-    int j = 0;
     Console.WriteLine("_____________________________");
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         Console.WriteLine("|   |   |   |   |   |   |   |");
         Console.WriteLine("|   |   |   |   |   |   |   |");
-
-
-        for (j = 0; j < arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
             Console.Write("|_" + arr[i, j] + "_");
         }
-
-
         Console.WriteLine("|");
+    }
+}
 
+*/
+// Массив поле для игры в тетрис
 
+string[,] playField = new string[,]
+{
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
+    { "                           |"},
 
+};
+// Отрисовывает массив поле для игры в тетрис
+void ShowArray(string[,] arr)
+{
+    Console.Clear();
+    Console.WriteLine("_____________________________");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.WriteLine("|                           |");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+
+            Console.Write("|" + arr[i, j]);
+        }
+        Console.WriteLine();
     }
 }
 
 ShowArray(playField);
+Console.WriteLine("|___________________________|");
 Console.WriteLine();
-
