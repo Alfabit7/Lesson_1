819d85da3ba8d1b40fb89cc071140dce47c394a2
Alfabit7 <tritg@yandex.ru>
2022-11-28 22:47:50 +0300

    change code for task60

--- Lesson_8/task58/Program.cs
+++ Lesson_8/task58/Program.cs
@@ -7,34 +7,31 @@
 15 18
 */
 
+int rows = new Random().Next(3, 5);
+int columns = new Random().Next(3, 5);
 
-// int rows = new Random().Next(3, 10);
-// int columns = new Random().Next(3, 10);
-int columns = 3;
-int rows = 3;
-// int[,] myArray = CreateArray(rows, columns);
-// int[,] myArray2 = CreateArray(rows, columns);
-
-int[,] myArray = {
-    { 1, 2, 2},
-    { 1, 2, 0},
-    { 2, 3, 3},
-};
-int[,] myArray2 = {
-    { 1, 2, 2},
-    { 2, 1, 0},
-    { 2, 1, 2}
-};
-
+int[,] myArray = CreateArray(rows, columns);
+int[,] myArray2 = CreateArray(rows, columns);
+Console.WriteLine();
+Console.WriteLine("Matrix A");
 PrintArray(myArray);
+Console.WriteLine();
+Console.WriteLine("Matrix B");
 PrintArray(myArray2);
-Console.WriteLine("_________");
-PrintArray(MultipliesMatrices(myArray, myArray2));
+Console.WriteLine();
+bool printOnNotPrint = false;
+int[,] newArray = MultipliesMatrices(myArray, myArray2);
+
+if (printOnNotPrint)
+{
+    Console.WriteLine("Matrix C");
+    PrintArray(newArray);
+}
+
 
 // Функция печатает массив
 void PrintArray(int[,] arr)
 {
-    Console.WriteLine("--------------------------");
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
@@ -47,77 +44,42 @@ void PrintArray(int[,] arr)
 
 }
 // Функция создает двумерный массив 
-// int[,] CreateArray(int rows, int columns)
-// {
-//     int[,] arr = new int[rows, columns];
-//     for (int i = 0; i < rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             arr[i, j] = new Random().Next(1, 4);
-//         }
-//     }
-//     return arr;
-// }
+int[,] CreateArray(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i, j] = new Random().Next(1, 4);
+        }
+    }
+    return arr;
+}
+
+//Функция перемножает две двумерные матрицы и вовдит результат если их можно перемножить
 int[,] MultipliesMatrices(int[,] arr, int[,] arr1)
 {
     int[,] result = new int[arr.GetLength(0), arr1.GetLength(1)];
-    int sum = 0;
-    int j = 0;
     if (arr.GetLength(1) != arr1.GetLength(0))
     {
-        Console.WriteLine("Перемножение невозможно количество столбцов первой матрицы не совпадает с количетвом строк второй матрицы ");
+        Console.WriteLine("Перемножение невозможно количество столбцов первой матрицы не совпадает с количеcтвом строк второй матрицы ");
     }
     else
     {
+        printOnNotPrint = true;
         for (int i = 0; i < arr.GetLength(0); i++)
         {
-            for (j = 0; j < arr.GetLength(1); j++)
+            for (int j = 0; j < arr.GetLength(1); j++)
             {
-                sum = sum + arr[i, j] * arr1[j, i];
-                result[i, j] = sum;
-                // Console.Write(sum + "! ");
-                // Console.Write(String.Join(";", result));
+                for (int k = 0; k < arr.GetLength(1); k++)
+                {
+                    result[i, j] += arr[i, k] * arr1[k, j];
+                }
             }
-            Console.WriteLine();
-            sum = 0;
         }
     }
-    // Console.Write(String.Join(";", result));
+
     return result;
 }
 
-
-
-
-/*
-
-
-{
-    "version": "0.2.0",
-    "configurations": [
-
-        {
-            // Use IntelliSense to find out which attributes exist for C# debugging
-            // Use hover for the description of the existing attributes
-            // For further information visit https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md
-            "name": ".NET Core Launch (console)",
-            "type": "coreclr",
-            "request": "launch",
-            "preLaunchTask": "build",
-            // If you have changed target frameworks, make sure to update the program path.
-            "program": "${workspaceFolder}/Lesson_8/task54/bin/Debug/net6.0/task54.dll",
-            "args": [],
-            "cwd": "${workspaceFolder}/Lesson_8/task54",
-            // For more information about the 'console' field, see https://aka.ms/VSCode-CS-LaunchJson-Console
-            "console": "internalConsole",
-            "stopAtEntry": false
-        },
-        {
-            "name": ".NET Core Attach",
-            "type": "coreclr",
-            "request": "attach"
-        }
-    ]
-}
-*/
---
+++ Lesson_8/task60/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+
+
+int y = new Random().Next(3, 10);
+int x = new Random().Next(3, 10);
+int z = new Random().Next(3, 10);
+
+// Проверяем что диапазон трехмерного массива не больше 99 
+while (x * y * z > 99)
+{
+    y = new Random().Next(3, 10);
+    x = new Random().Next(3, 10);
+    z = new Random().Next(3, 10);
+}
+// Создаем трехмерный рандомный массив 
+int[,,] myArray = CreateThreeArray(x, y, z);
+// Выводим массив на экран
+PrintThreeArray(myArray);
+
+
+// Функция генерация трехмерного рандомного массива 
+int[,,] CreateThreeArray(int y, int x, int z)
+{
+    int[,,] arr = new int[y, x, z];
+    // m двузначные значения, которыми будет заполнятся массив от 10 до 99 
+    int m = 10;
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            for (int k = 0; k < z; k++)
+            {
+                arr[i, j, k] = m;
+                m++;
+            }
+        }
+    }
+    return arr;
+}
+
+// Функция выводит на экран трехмерный массив 
+int[,,] PrintThreeArray(int[,,] arr)
+
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($" {arr[i, j, k],5:f2} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    return arr;
+}
