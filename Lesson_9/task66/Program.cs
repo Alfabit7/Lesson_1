f3ee59a6b4cf5cc9b52ce7b1bdfef61303eb74ef
Alfabit7 <tritg@yandex.ru>
2022-12-02 20:02:13 +0300

    added folder with Lesson_9

---
+++ Lesson_9/task66/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+
+int numM;
+int numN;
+int sum = 0;
+
+RequestUserNumber();
+int result = sumMandN(numM);
+Console.Write(result);
+
+
+void RequestUserNumber()
+{
+    Console.Write("Введите число M: ");
+    numM = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число N, которое больше, предыдущего числа M: ");
+    numN = Convert.ToInt32(Console.ReadLine());
+    if (numM > numN) { RequestUserNumber(); }
+}
+
+int sumMandN(int M)
+{
+    if (M <= numN)
+    {
+        sum += M;
+        sumMandN(M + 1);
+    }
+    return sum;
+}
+
+
---
+++ Lesson_9/task68/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) { return result = n + 1; }
+
+    else if (m > 0 && n == 0) { return result = Ackermann(m - 1, 1); }
+
+    else { return result = Ackermann(m - 1, Ackermann(m, n - 1)); }
+}
+Ackermann(m, n);
+Console.Write("result= " + result);
